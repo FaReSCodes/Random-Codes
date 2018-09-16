9b4e0cdc2b705646f361b9e9fc068362ee3b3139
FaReSCodes <43314308+FaReSCodes@users.noreply.github.com>
2018-09-16 14:31:59 +0300

    Create inrole-info.js

---
+++ inrole-info.js
@@ -0,0 +1,30 @@
+client.on('message', eyadcodes =>{
+    var eyad = 0;
+    var codes = "!" , codees = 'inrole';
+if(eyadcodes.content.startsWith(codes + codees)) {
+    const args = eyadcodes.content.split(' ').slice(1).join(' ');
+    const role = eyadcodes.guild.roles.find('name' , args);
+    if(!args) return;
+    if(!role) return eyadcodes.channel.send('**قد يكون أسم الرتبه خطأ او ليست موجوده !!**')
+    let eyadtxt= "";
+     eyadcodes.guild.members.filter(m => m.roles.has(role.id)).forEach(xx => {
+        eyad++;
+        eyadtxt += `\`${eyad}\` - <@${xx.id}> \n`;
+    });
+
+    if(eyadtxt == "") {
+        eyadtxt = "لا يوجد احد في هذه الرتبة";
+    } else {
+        eyadtxt = eyadtxt;
+    };
+
+    eyadcodes.channel.send({embed: new Discord.RichEmbed()
+    .setThumbnail(eyadcodes.author.avatarURL)
+    .setAuthor(eyadcodes.guild.name , eyadcodes.guild.iconURL)
+            .setDescription(`**Users in this role ${role} \n\n${eyadtxt}**`)
+            .setFooter(eyadcodes.guild.name)
+            .setColor('RANDOM')
+            .setTimestamp()
+    })
+}
+})
