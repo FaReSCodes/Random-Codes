03308df5f1972a97e46ebe6199c2fa99b1d598fd
FaReSCodes <43314308+FaReSCodes@users.noreply.github.com>
2018-09-16 14:38:56 +0300

    Create link.js

---
+++ link.js
@@ -0,0 +1,18 @@
+  client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 2,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
+
+message.author.send(`**مدة الرابط : يـوم
+عدد استخدامات الرابط : 2**`)
+
+
+    }
+});
